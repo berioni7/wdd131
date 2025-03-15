1df635734af2892f13591c941358426b863722ee
berioni7 <gberioni@byupathway.edu>
2025-03-14 21:42:48 -0300

    temple album added

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerBtn = document.getElementById("hamburgerBtn");
+    const menu = document.querySelector(".menu");
+
+    hamburgerBtn.addEventListener("click", function() {
+        menu.classList.toggle("show"); 
+
+        
+        if (menu.classList.contains("show")) {
+            hamburgerBtn.innerHTML = "&#10006;"; 
+        } else {
+            hamburgerBtn.innerHTML = "&#9776;"; 
+        }
+    });
+});
